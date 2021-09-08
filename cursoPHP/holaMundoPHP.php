2fb282e8925fdba725d98962adf5c862e61ac194
Carolina <carolina.bascunan1601@alumnos.ubiobio.cl>
2021-09-08 00:36:26 -0300

    Hola mundo PHP

---
+++ cursoPHP/holaMundoPHP.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "Hola Mundo";
+
+//echo noa sirve para mostrar informacion en pantalla
+
+# otro comentario 
+
+/*
+ o así comentar
+*/
+
+?>
+
+
+<!Doctype html>
+<html lang = "en">
+<head>
+    <meta charset = "UTF-8">
+    <title> Documento </title>
+</head> 
+<body>
+    <h1> <?php echo "Hola  Mundo"; ?> </h1>
+</body>
+</html>
